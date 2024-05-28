3e76972984e739ff429cbc33cf84068eed24828c
Raiyan Islam <Raiyan.islam2@student.tdsb.on.ca>
2024-05-28 01:23:10 -0400

    modified:   Invaders/invaders.py

--- Invaders/invaders.py
+++ Invaders/invaders.py
@@ -1,8 +1,10 @@
 import pgzrun
-from pgzero.builtins import Actor, clock, animate, sounds, keys, keyboard
+from pgzero.builtins import Actor, clock, animate, sounds, keys, keyboard, Rect
 from pgzhelper import * 
 import random
 import os
+
+### DEFINITIONS
 
 # Window Settings
 os.environ['SDL_VIDEO_CENTERED'] = '1' #Forces window to be centered on screen.
@@ -14,11 +16,9 @@ ALIENS_PER_ROW = 9
 score = 0
 lives = 3
 difficulty = 0
-is_playing = False
-has_game_started = False
-paused = False
-
-play_again = Rect(((800 * 1 / 3, 200), (800 / 3, 200)))
+game_active = False
+game_started = False
+game_paused = False
 
 # Initialize primary Actors
 player = Actor('playership2_orange')
@@ -26,32 +26,35 @@ player.pos = (WIDTH // 2, HEIGHT - 50)
 player.death_frame = 0
 player.scale = 0.5 #resizing sprite to something more reasonable
 
+# UFO Actor
 ufo = Actor('enemygreen1', pos=(50, 50))
 ufo.points = 100
 ufo.is_waiting = False
 
+# Alien Variables
 aliens = []
 alien_bullets = []
-furthest_alien = None
 alien_direction = 20
 alien_delay = 1
-max_height = 450
+alien_max_height = 450
 
-alien_color_index = {
-    'green': 150,
-    'black': 200,
-    'blue': 250,
-    'red': 300
-}
+# Other variables
+shields = []
+bullet = None
+view_box = Rect(((WIDTH / 3, HEIGHT / 3), (WIDTH / 3, HEIGHT / 3)))
 
+### HELPER FUNCTIONS
+
+# Returns a random image asset based on the prefix, a list of colors, and the min-max image
 def generate_random_tag(prefix, colors, minIndex=0, maxIndex=0):
     color = random.choice(colors)
+    
+    # Handle image assets with index
     if minIndex != maxIndex:
         index = random.randint(minIndex, maxIndex)
-        digits = len(str(maxIndex))
+        digits = len(str(maxIndex)) # get amount of digits
         return f'{prefix}{color}{index:0{digits}d}', color, index
-    return f'{prefix}{color}', color
-        
+    return f'{prefix}{color}', color    
 
 def set_ufo():
     ufo.right = 0
@@ -59,15 +62,22 @@ def set_ufo():
     ufo.image = tag
     ufo.scale = 0.5
     
-set_ufo()
+def move_ufo():
+    if ufo.left < WIDTH:
+        ufo.x += 1
+        ufo.is_waiting = False
+    elif not ufo.is_waiting:
+        next_time = random.randint(10, 20)
+        clock.schedule(set_ufo, next_time)
+        ufo.is_waiting = True    
 
-shields = []
+
 def spawn_shield(pos):
     shield = Actor('shield3', pos=pos)
     shield.level = 3
     shields.append(shield)
 
-def draw_row_aliens(type, y, points):    
+def draw_row_aliens(type, y, points):
     global aliens
     for x in range(ALIENS_PER_ROW):
         a = Actor(type)
@@ -77,12 +87,18 @@ def draw_row_aliens(type, y, points):
         a.points = points
         aliens.append(a)
         
-def draw_all_aliens(difficulty):
-    last_y = 100
+def draw_all_aliens(difficulty, y=100):
+    alien_color_index = {
+        'green': 150,
+        'black': 200,
+        'blue': 250,
+        'red': 300
+    }
+    row_y = y
     for i in range(difficulty):
         tag, color, _ = generate_random_tag('enemy', ['green', 'red', 'blue', 'black'], 1, 5)
-        draw_row_aliens(tag, last_y, points=alien_color_index[color])
-        last_y += 50
+        draw_row_aliens(tag, row_y, points=alien_color_index[color])
+        row_y += 50
 
 def determine_direction():
     global aliens, alien_direction
@@ -113,6 +129,9 @@ def spawn_alien_bullet():
 def move_aliens():
     global alien_direction, aliens, alien_delay
     
+    if game_paused or not game_active:
+        return
+    
     direction_change = determine_direction()
     if direction_change:
         alien_direction *= -1
@@ -122,7 +141,7 @@ def move_aliens():
     for alien in aliens:
         if direction_change:
             alien.y += 50
-            below_max_height |= alien.bottom > max_height
+            below_max_height |= alien.bottom > alien_max_height
         else:
             alien.x += alien_direction
     
@@ -131,9 +150,10 @@ def move_aliens():
         return
     
     spawn_alien_bullet()
-        
-    if is_playing:
-        clock.schedule(move_aliens, alien_delay)    
+    
+    # clock.schedule(move_aliens, alien_delay)    
+    
+clock.schedule_interval(move_aliens, alien_delay)
         
 def damage_shield(alien_bullet):
     for shield in shields:
@@ -150,20 +170,20 @@ def damage_shield(alien_bullet):
     return False
 
 def animate_death():
-    global player, is_playing, paused
+    global player, game_active, game_paused
     if player.death_frame > 2:
-        paused = False
+        game_paused = False
         player.image = 'playership2_orange'
         player.scale = 0.5
         player.death_frame = 0
     else:
-        paused = True
+        game_paused = True
         player.death_frame += 1
         player.image = f'star{player.death_frame}'
         clock.schedule_unique(animate_death, 0.3)
 
 def handle_alien_bullets():
-    global lives, is_playing
+    global lives, game_active
     for alien_bullet in alien_bullets:
         alien_bullet.y += 5
         
@@ -184,42 +204,38 @@ def handle_alien_bullets():
             
 def handle_player_bullets():
     global bullet, score
+    bullet.y -= 5   
+         
     if ufo.colliderect(bullet):
-            ufo.left = WIDTH
-            score += ufo.points
-
-    bullet.y -= 5        
-    for idx, alien in enumerate(aliens):
+        ufo.left = WIDTH
+        score += ufo.points
+        bullet = None
+        return
+    
+    if bullet and bullet.bottom < 0:
+        bullet = None
+        return
+    
+    for alien in aliens:
         if bullet.colliderect(alien):
-            aliens.pop(idx)
+            aliens.remove(alien)
             score += alien.points
             bullet = None
             return
 
-    if bullet and bullet.bottom < 0:
-        bullet = None
-
-def move_ufo():
-    if ufo.left < WIDTH:
-        ufo.x += 1
-        ufo.is_waiting = False
-    elif not ufo.is_waiting:
-        next_time = random.randint(10, 20)
-        clock.schedule(set_ufo, next_time)
-        ufo.is_waiting = True
         
 def handle_lose_condition():
-    global is_playing, bullet
-    is_playing = False
+    global game_active, bullet
+    game_active = False
     clock.unschedule(move_aliens)
     alien_bullets.clear()
     bullet = None
     player.image = 'star3'
 
 def reset_game():
-    global lives, score, is_playing, difficulty, alien_direction, alien_delay
+    global lives, score, game_active, difficulty, alien_direction, alien_delay
     alien_direction = 20
-    is_playing = True
+    game_active = True
     lives = 3
     score = 0
     difficulty = 1
@@ -239,32 +255,30 @@ def reset_game():
 
 
 def draw_game_screen():
-    if not is_playing:
-        screen.draw.filled_rect(play_again, (0, 0, 0))
-        screen.draw.rect(play_again, (255, 255, 255))
+    if not game_active:
+        screen.draw.filled_rect(view_box, (0, 0, 0))
+        screen.draw.rect(view_box, (255, 255, 255))
         screen.draw.text('play moron', center=(400, 300))
-        if has_game_started:
+        if game_started:
             screen.draw.text(f'HIGHSCORE: {score}', center=(400, 250))
-
-bullet = None
 
 # Event Handlers - Handle one-time input
 def on_mouse_down(pos, button): 
-    global has_game_started, is_playing
-    if play_again.collidepoint(pos) and not is_playing:
+    global game_started, game_active
+    if view_box.collidepoint(pos) and not game_active:
         reset_game()
-        has_game_started = True
+        game_started = True
         
 def on_key_down(key, unicode):
-    global bullet, is_playing    
-    if key == keys.SPACE and bullet == None and is_playing and not paused:
+    global bullet, game_active    
+    if key == keys.SPACE and bullet == None and game_active and not game_paused:
         bullet = Actor('laserblue07', player.pos)
 
 # Update - Handle ongoing input, update positions, check interactions
 def update():
-    global bullet, is_playing, score
+    global bullet, game_active, score
     
-    if not is_playing or paused:
+    if not game_active or game_paused:
         return
     
     if (keyboard[keys.LEFT] or keyboard[keys.A]) and player.left > 0:
@@ -304,4 +318,6 @@ def draw():
 
 
 # Setup, Scheduling, and Go:
+set_ufo()
+
 pgzrun.go()
